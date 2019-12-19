f561e8fc35e78e8aeea261a9c849a103e8ad33b7
PedrassoxD <pedroperlab@gmail.com>
2019-12-20 00:05:14 +0100

    new revision

--- OfficeApp_Pedro/src/app/models/material.model.ts
+++
@@ -1,9 +0,0 @@
-export class Material {
-  constructor(
-    public id: string,
-    public name: string,
-    public description: string,
-    public quantity: number,
-    public image: string
-  ) {}
-}
